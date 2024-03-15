2f92629f6a548a7eab3f1f72c4e098ed460b45d4
Sophia Yoo <sophiayoo@princeton.edu>
2024-03-15 01:15:39 +0000

    Update config jc5.

--- ebpf/configure_jc5.sh
+++ ebpf/configure_jc5.sh
@@ -2,8 +2,8 @@
 
 # Network interface configuration
 INTERFACE="enp3s0f1"
-IP_ADDRESS="130.0.0.5"
-HW_ADDRESS="00:00:00:00:00:82"
+IP_ADDRESS="129.0.0.5"
+HW_ADDRESS="00:00:00:00:00:81"
 GATEWAY="1.0.0.1"
 ARP_ADDRESS="00:00:00:00:00:01"
 
@@ -16,9 +16,9 @@ sudo route add $GATEWAY dev $INTERFACE
 sudo arp -s $GATEWAY $ARP_ADDRESS
 
 # Add route entries
-sudo route add -net 128.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 129.0.0.0/8 gw $GATEWAY dev $INTERFACE
 sudo route add -net 136.0.0.0/8 gw $GATEWAY dev $INTERFACE
-sudo route add -net 130.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 131.0.0.0/8 gw $GATEWAY dev $INTERFACE
 
 # Turn off TX and RX checksum offloading
 sudo ethtool -K $INTERFACE tx off rx off
