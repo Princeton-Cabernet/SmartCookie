44ea438553d5d893066c0395132b00e686663d88
Sophia Yoo <sophiayoo@princeton.edu>
2024-03-20 00:35:06 +0000

    Update config files with attack server routing info.

--- ebpf/configure/configure_jc5.sh
+++ ebpf/configure/configure_jc5.sh
@@ -17,8 +17,11 @@ sudo arp -s $GATEWAY $ARP_ADDRESS
 
 # Add route entries
 sudo route add -net 129.0.0.0/8 gw $GATEWAY dev $INTERFACE
-sudo route add -net 136.0.0.0/8 gw $GATEWAY dev $INTERFACE
 sudo route add -net 131.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 144.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 152.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 160.0.0.0/8 gw $GATEWAY dev $INTERFACE
+sudo route add -net 168.0.0.0/8 gw $GATEWAY dev $INTERFACE
 
 # Turn off TX and RX checksum offloading
 sudo ethtool -K $INTERFACE tx off rx off
